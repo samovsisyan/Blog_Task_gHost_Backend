222459887403dbc352a110522e68a8d20338ae2a
samovsisyan <samovsisyan9@gmail.com>
2020-02-11 01:45:13 +0400

    Crate Admin Page

--- routes/admin.js
+++ routes/admin.js
@@ -3,31 +3,40 @@ const models = require('../models/Blog');
 const express = require('express');
 const router = express.Router();
 
-
-
-
 /* GET blog listing. */
-router.post('/', async (req, res, next) => {
+
+router.get('/', async (req, res, next) => {
     try {
-        const {title, description, short_description, slug} = req.body;
+        const blog = await models.findAll({});
 
-        await models.create({
-            title,
-            description,
-            short_description,
-            slug,
-        });
 
-        res.send({
-            status: "ok",
-            message: "Create New Blog"
-        })
-
+        res.render('admin/Admin', {blog: blog});
     } catch (e) {
         next(e)
-
     }
 });
+
+// router.post('/', async (req, res, next) => {
+//     try {
+//         const {title, description, short_description, slug} = req.body;
+//
+//         await models.create({
+//             title,
+//             description,
+//             short_description,
+//             slug,
+//         });
+//
+//         res.send({
+//             status: "ok",
+//             message: "Create New Blog"
+//         })
+//
+//     } catch (e) {
+//         next(e)
+//
+//     }
+// });
 
 
 
