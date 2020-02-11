ab7a1b037711777028e9687b7849a9d09677305b
samovsisyan <samovsisyan9@gmail.com>
2020-02-12 03:31:29 +0400

    not working multi language

--- views/Home.jsx
+++ views/Home.jsx
@@ -6,48 +6,88 @@ const Wrapper = require('./Wrapper');
 // import Section from './pages/Section';
 // const BlogDetails = require('./pages/BlogDetails');
 
-import {I18nProvider, LOCALES} from './i18n';
+// import {I18nProvider, LOCALES} from './i18n';
 // import { FormattedMessage } from 'react-intl';
 
-import translate from "./i18n/translate";
+// import translate from "./i18n/translate";
+import counterpart from 'counterpart';
+
+import Translate from 'react-translate-component'
+import en from "./lang/en";
+import ru from "./lang/ru";
+
+counterpart.registerTranslations('en', en);
+counterpart.registerTranslations('ru', ru);
+
+counterpart.setLocale('en');
+
+
+const Link = (props) => {
+    return (
+    <Translate
+        content={props.content}
+        component="a"
+        href="//google.com"
+        target= "_blank"
+    />
+)
+}
 
 class Home extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                locale: LOCALES.ELNGLISH,
-            }
-        }
-
-        handleClick = () => {
-            this.setState({
-                locale: LOCALES.FRENCH
-            })
-            console.log(this.state.locale,4444444444444444)
-        }
+constructor(props) {
+    super(props);
+    this.state = {
+        lang: "en"
+    };
+}
 
 
+    onLangChange (e) {
+        this.setState({lang: e.target.value})
+        counterpart.setLocale(e.target.value);
+
+    }
 
     render() {
-
-    // const [locale, setLocale] = useState(LOCALES.ELNGLISH);
-
-    const {locale, setLocale} = this.state;
+        const link =<Link content="link" />
+        console.log(this.state,555555555555555555)
+        // const placeholder = counterpart.translate("placeholder")
         return (
             <Wrapper>
-                <I18nProvider locale = {locale}>
-                    {/*<Admin />*/}
-                    {/*<Section />*/}
-                    {/*<FormattedMessage id='hello' />*/}
-                    {translate("hello")}
-                    <h1>Hello Home </h1>
-                    {translate("edit", {path: <code>src/App.jsx</code>})}
-                    <button onClick={this.handleClick()}>FRENCH</button>
-                    {/*<button onClick={()=> setLocale(LOCALES.ELNGLISH)}>ELNGLISH</button>*/}
-                    {/*<button onClick={()=> setLocale(LOCALES.FRENCH)}>FRENCH</button>*/}
+                <h1>Hello Home react-translate-component</h1>
+
+                <select value={this.state.lang} onChange={this.onLangChange}>
+                    <option value="en">EN</option>
+                    <option value="ru">RU</option>
+                </select>
+                
+                   <Translate content="title"  component ="h1" className="class" />
+
+                   <Translate content="copy.p1"  component ="p" unsafe={true} />
+
+                   <Translate content="copy.p2" component ="p" with={{link}} />
 
 
-                </I18nProvider>
+                   <Translate component ="input"  type="text" attributes={{placeholder: "placeholder"}} />
+
+
+
+                {/*<I18nProvider locale = {locale}>*/}
+                {/*    /!*<Admin />*!/*/}
+                {/*    /!*<Section />*!/*/}
+                {/*    /!*<FormattedMessage id='hello' />*!/*/}
+                {/*    {translate("hello")}*/}
+                {/*    <h1>Hello Home </h1>*/}
+                {/*    {translate("edit", {path: <code>src/App.jsx</code>})}*/}
+                {/*    <button onClick={this.handleClick()}>FRENCH</button>*/}
+                {/*    /!*<button onClick={()=> setLocale(LOCALES.ELNGLISH)}>ELNGLISH</button>*!/*/}
+                {/*    /!*<button onClick={()=> setLocale(LOCALES.FRENCH)}>FRENCH</button>*!/*/}
+
+
+                {/*</I18nProvider>*/}
+
+
+
             </Wrapper>
         )
     }
---
+++ views/lang/en.jsx
@@ -0,0 +1,10 @@
+export default {
+    title: 'Titile',
+
+    copy: {
+        p1: 'Paragraph with <b>html</b> inside',
+        p2: 'Paragraph with clickable %(link)s',
+    },
+    link: "link",
+    placeholder: "Input with placeholder",
+}
