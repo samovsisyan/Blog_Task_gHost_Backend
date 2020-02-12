9a649ed2621da13de4b2feaa8aef664ef00c1bed
samovsisyan <samovsisyan9@gmail.com>
2020-02-12 11:24:51 +0400

    Create authorizetion and jwt

--- app.js
+++ app.js
@@ -7,12 +7,66 @@ const indexRouter = require('./routes/index');
 const allowOrigin = require('./middleware/allowOrigin');
 
 
+const jwt = require('jsonwebtoken')
+
 const app = express();
 
+app.get('/api', (req, res) => {
+    res.json({
+        message: "Welcome to the Api"
+    });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {
+   jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        }else {
+            res.json({
+                message: "Post creates...",
+                authData
+            })
+        }
+   });
+
+});
+
+
+app.post("/api/login", (req, res) => {
+    const user = {
+        id: 1,
+        username: "brad ",
+        email: "brad@gmail.com",
+    }
+
+    jwt.sign({user: user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        })
+    });
+});
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+
+        const bearerToken = bearer[1];
+
+        req.token = bearerToken;
+
+        next();
+    }else {
+        res.sendStatus(403);
+    }
+}
+
+app.listen(5000, () => console.log("Server started on port 5000"))
+
+
+
 const profile = require('./routes/profile');
-
-
-
 
 
 
